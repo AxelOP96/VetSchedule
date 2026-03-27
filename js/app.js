56a56df624de7506246fc3ede886ee8222e12d33
AxelOP96 <axeelleguero@gmail.com>
2026-03-26 22:09:58 -0300

    Events

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const d = document;
 const pacienteInput = d.querySelector('#paciente');
-const propiertarioInput = d.querySelector('#propietario')
+const propietarioInput = d.querySelector('#propietario')
 const emailInput = d.querySelector('#email')
 const fechaInput = d.querySelector('#fecha')
 const sintomasInput = d.querySelector('#sintomas')
@@ -12,3 +12,19 @@ const citaObj = {
     fecha: '',
     sintomas: ''
 }
+
+pacienteInput.addEventListener('change', (e) =>{
+    citaObj[e.target.name] = e.target.value;
+})
+propietarioInput.addEventListener('change', (e) =>{
+    citaObj[e.target.name] = e.target.value;
+})
+emailInput.addEventListener('change', (e) =>{
+    citaObj[e.target.name] = e.target.value;
+})
+fechaInput.addEventListener('change', (e) =>{
+    citaObj[e.target.name] = e.target.value;
+})
+sintomasInput.addEventListener('change', (e) =>{
+    citaObj[e.target.name] = e.target.value;
+})
