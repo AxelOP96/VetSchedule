590e0c942d9273458b6f48d4cec302545b5c0ddc
AxelOP96 <axeelleguero@gmail.com>
2026-03-27 00:12:55 -0300

    Notification class and alert added

--- js/app.js
+++ js/app.js
@@ -36,8 +36,40 @@ function submitCita(e) {
     e.preventDefault();
     const  {paciente, propietario, email, fecha, sintomas} = citaObj;
     if(Object.values(citaObj).some(valor => valor.trim() === '')){
+        const notification = new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
+    
         return
     }
         
 
+}
+
+class Notificacion {
+
+    constructor({texto, tipo}) {
+        this.texto= texto
+        this.tipo= tipo
+
+        this.mostrar();
+    }
+    mostrar(){
+        const alerta = d.createElement('div');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        const alertaPrevia = d.querySelector('.alert')
+        alertaPrevia?.remove();
+
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        alerta.textContent = this.texto;
+
+        formulario.parentElement.insertBefore(alerta, formulario)
+
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000)
+    }
 }
