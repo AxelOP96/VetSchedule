24eab2a1efb466bc3bea49ce51d914a6f15e667c
AxelOP96 <axeelleguero@gmail.com>
2026-03-26 21:49:07 -0300

    Variables and object added

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const d = document;
+const pacienteInput = d.querySelector('#paciente');
+const propiertarioInput = d.querySelector('#propietario')
+const emailInput = d.querySelector('#email')
+const fechaInput = d.querySelector('#fecha')
+const sintomasInput = d.querySelector('#sintomas')
+
+const citaObj = {
+    paciente: '',
+    propietario : '',
+    email: '',
+    fecha: '',
+    sintomas: ''
+}
