98c96a03e2d4b8ca345ed2ed58cbc963c1fba03d
AxelOP96 <axeelleguero@gmail.com>
2026-03-29 17:26:45 -0300

    Adding IndexedDB

--- js/app.js
+++ js/app.js
@@ -1,21 +1,11 @@
 import { pacienteInput, propietarioInput, emailInput, fechaInput, sintomasInput, formulario} from './selectores.js'
-import { submitCita } from './funciones.js';
 import { citaObj } from './variables.js';
+import { eventListeners, crearDB } from './funciones.js';
 const d = document
-pacienteInput.addEventListener('change', (e) =>{
-    citaObj[e.target.name] = e.target.value;
-})
-propietarioInput.addEventListener('change', (e) =>{
-    citaObj[e.target.name] = e.target.value;
-})
-emailInput.addEventListener('change', (e) =>{
-    citaObj[e.target.name] = e.target.value;
-})
-fechaInput.addEventListener('change', (e) =>{
-    citaObj[e.target.name] = e.target.value;
-})
-sintomasInput.addEventListener('change', (e) =>{
-    citaObj[e.target.name] = e.target.value;
-})
-formulario.addEventListener('submit', submitCita);
+
+window.onload = () =>{
+    eventListeners();
+    crearDB();
+}
+
 
--- js/funciones.js
+++ js/funciones.js
@@ -3,6 +3,7 @@ import AdminCitas from "./classes/AdminCitas.js";
 import { citaObj, editando } from "./variables.js";
 import { formulario , formularioInput, pacienteInput, propietarioInput, emailInput, fechaInput, sintomasInput} from "./selectores.js";
 const citas = new AdminCitas();
+let DB;
 export function submitCita(e) {
     e.preventDefault();
     const  {paciente, propietario, email, fecha, sintomas} = citaObj;
@@ -70,4 +71,51 @@ export function cargarEdicion(cita) {
 
     formularioInput.value = 'Guardar Cambios'
 }
+export function eventListeners(){
+    pacienteInput.addEventListener('change', (e) =>{
+        citaObj[e.target.name] = e.target.value;
+    })
+    propietarioInput.addEventListener('change', (e) =>{
+        citaObj[e.target.name] = e.target.value;
+    })
+    emailInput.addEventListener('change', (e) =>{
+        citaObj[e.target.name] = e.target.value;
+    })
+    fechaInput.addEventListener('change', (e) =>{
+        citaObj[e.target.name] = e.target.value;
+    })
+    sintomasInput.addEventListener('change', (e) =>{
+        citaObj[e.target.name] = e.target.value;
+    })
+    formulario.addEventListener('submit', submitCita);
+}
 
+export function crearDB(){
+    const crearDB = window.indexedDB.open('citas', 1);
+    crearDB.onerror = function(){
+        console.log('Hubo un error');
+    }
+
+    crearDB.onsuccess = function() {
+        console.log('BD creada')
+        DB = crearDB.result;
+    }
+
+    crearDB.onupgradeneeded = function(e) {
+        const db = e.target.result;
+
+        const objectStore = db.createObjectStore('citas', {
+            keyPath: 'id',
+            autoIncrement: true
+        });
+
+        objectStore.createIndex('paciente', 'paciente', {unique: false});
+        objectStore.createIndex('propietario', 'propietario', {unique: false});
+        objectStore.createIndex('telefono', 'telefono', {unique: false});
+        objectStore.createIndex('fecha', 'fecha', {unique: false});
+        objectStore.createIndex('sintomas', 'sintomas', {unique: false});
+        objectStore.createIndex('id', 'id', {unique: true});
+
+        console.log('DB creada y lista...')
+    }
+}
