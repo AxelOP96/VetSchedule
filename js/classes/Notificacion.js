3e624a90b66537c700a99358bdb8c38501de350f
AxelOP96 <axeelleguero@gmail.com>
2026-03-29 00:02:06 -0300

    start of migrating code using classes

---
+++ js/classes/Notificacion.js
@@ -0,0 +1,26 @@
+export default class Notificacion {
+
+    constructor({texto, tipo}) {
+        this.texto= texto
+        this.tipo= tipo
+
+        this.mostrar();
+    }
+    mostrar(){
+        const alerta = d.createElement('div');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        const alertaPrevia = d.querySelector('.alert')
+        alertaPrevia?.remove();
+
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        alerta.textContent = this.texto;
+
+        formulario.parentElement.insertBefore(alerta, formulario)
+
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000)
+    }
+}
